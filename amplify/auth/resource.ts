db45d4b7ecd27dbf7e06f3c8f7524079f218e1b6
Lucas <lucaspingpao@gmail.com>
2024-12-29 18:35:14 -0600

    added per-user data isolation

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,22 +9,3 @@ export const auth = defineAuth({
     email: true,
   },
 });
-
-import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
-
-const schema = a.schema({
-  Todo: a.model({
-    content: a.string(),
-  }).authorization(allow => [allow.owner()]),
-});
-
-export type Schema = ClientSchema<typeof schema>;
-
-export const data = defineData({
-  schema,
-  authorizationModes: {
-    // This tells the data client in your app (generateClient())
-    // to sign API requests with the user authentication token. 
-    defaultAuthorizationMode: 'userPool',
-  },
-});
