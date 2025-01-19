0f8690b0495e0a56a28f834d307d6afce26e6bec
Lucas <lucaspingpao@gmail.com>
2025-01-18 20:19:15 -0500

    update lambda function from aws console

--- my-lambda-function-py/lambda_function.py
+++ my-lambda-function-py/lambda_function.py
@@ -34,19 +34,89 @@ logger.info("SUCCESS: Connection to RDS for MySQL instance succeeded")
 
 
 def lambda_handler(event, context):
-    # Fetch all data from the Stats table and return it as JSON.
+    logger.info("event: %s, context: %s", event, context)
+    http_method = event.get('httpMethod')
+    path = event.get('resource')
+    query_params = event.get('queryStringParameters', {})
+    body = event.get('body')
+
+    if http_method == 'POST':
+        if path == '/stats':
+            return post_stats(body)
+        if path == '/users':
+            return post_user(body)
+        else:
+            return {
+                "statusCode": 404,
+                "body": json.dumps({"message": "Not Found"})
+            }
+
+    elif http_method == 'PUT':
+        if path == '/users':
+            return put_user(body)
+        else:
+            return {
+                "statusCode": 404,
+                "body": json.dumps({"message": "Not Found"})
+            }
+        
+    elif http_method == 'GET':
+        if path == '/users':
+            return get_user()
+        elif path == '/stats':
+            return get_stats()
+        else:
+            return {
+                "statusCode": 404,
+                "body": json.dumps({"message": "Not Found"})
+            }
+    
+    return {
+        "statusCode": 404,
+        "body": json.dumps({"message": "Not Found"})
+    }
+
+def get_user():
+    logger.info("Fetching users from Users table")
     try:
         with conn.cursor() as cur:
-            # Fetch all rows from the Stats table
-            cur.execute("SELECT * FROM stats")
+            cur.execute("SELECT user_id, username FROM users")
+            users = cur.fetchall()
+
+        logger.info("Fetched data from Users table:")
+        logger.info(users)
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Data fetched successfully", "data": users})
+        }
+
+    except Exception as e:
+        logger.error("Error fetching data from Users table:")
+        logger.error(e)
+
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "Error fetching data", "error": str(e)})
+        }
+
+def get_stats():
+    logger.info("/stats GET method called")
+    try:
+        with conn.cursor() as cur:
+            cur.execute("SELECT score, username, created_at FROM stats LEFT JOIN users ON stats.user_id = users.user_id ORDER BY score DESC LIMIT 10")
             stats = cur.fetchall()
 
-        # Log and return the data
         logger.info("Fetched data from Stats table:")
         logger.info(stats)
 
         stats_serializable = [
-            (id, user_id, score, created_at.isoformat()) for id, user_id, score, created_at in stats
+            {
+                "score": score,
+                "username": username,
+                "created_at": created_at.isoformat()
+            }
+            for score, username, created_at in stats
         ]
 
         return {
@@ -61,4 +131,105 @@ def lambda_handler(event, context):
         return {
             "statusCode": 500,
             "body": json.dumps({"message": "Error fetching data", "error": str(e)})
+        }
+
+def post_stats(body):
+    logger.info("/stats POST method called")
+    try:
+        user_id = body.get('user_id')
+        score = body.get('score')
+
+        if user_id is None or score is None:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"message": "Invalid request body"})
+            }
+
+        created_at = datetime.now()
+
+        with conn.cursor() as cur:
+            cur.execute("INSERT INTO stats (user_id, score, created_at) VALUES (%s, %s, %s)", (user_id, score, created_at))
+            conn.commit()
+
+        logger.info("Data inserted into Stats table successfully")
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Data inserted successfully"})
+        }
+
+    except Exception as e:
+        logger.error("Error inserting data into Stats table:")
+        logger.error(e)
+
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "Error inserting data", "error": str(e)})
+        }
+
+def post_user(body):
+    logger.info("/users POST method called")
+
+    try:
+        user_id = body.get('user_id')
+        email = body.get('email')
+        if user_id is None or email is None:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"message": "Invalid request body"})
+            }
+
+        with conn.cursor() as cur:
+            cur.execute("SELECT * FROM users WHERE user_id = %s", (user_id))
+            existing_user = cur.fetchone()
+            if not existing_user:
+                cur.execute("INSERT INTO users (user_id, email, username) VALUES (%s, %s, %s)", (user_id, email, None))
+                conn.commit()
+
+        logger.info("Data inserted into Users table successfully")
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Data inserted successfully"})
+        }
+
+    except Exception as e:
+        logger.error("Error inserting data into Users table:")
+        logger.error(e)
+
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "Error inserting data", "error": str(e)})
+        }
+
+def put_user(body):
+    logger.info("/users PUT method called")
+    try:
+        user_id = body.get('user_id')
+        username = body.get('username')
+
+        if user_id is None or username is None:
+            return {
+                "statusCode": 400,
+                "body": json.dumps({"message": "Invalid request body"})
+            }
+
+        with conn.cursor() as cur:
+            cur.execute("UPDATE users SET username = %s WHERE user_id = %s", (username, user_id))
+            conn.commit()
+
+        logger.info("Data updated in Users table successfully")
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "Data updated successfully"})
+        }
+
+    except Exception as e:
+        logger.error("Error updating data in Users table:")
+        logger.error(e)
+
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"message": "Error updating data", "error": str(e)})
         }
